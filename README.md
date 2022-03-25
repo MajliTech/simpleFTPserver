47e5290f13c839ba6324ec8cbebfa973dc920490
Miłosz <hello@majlitech.tk>
2022-03-25 16:56:49 +0100

    First sus

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# simpleFTPserver
- A simple ftp server written in python
+## simpleFTPserver
+# A simple ftp server written in python
+Q: How to use it?
+A: Install requierments.txt, hange users in config.json and start main.py. 
---
+++ main.py
@@ -0,0 +1,76 @@
+from plistlib import InvalidFileException
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import TLS_FTPHandler
+from pyftpdlib.servers import FTPServer
+import elevate
+import os
+import json
+
+
+
+def main():
+    #Config file
+    if os.path.isfile("config.json"):
+        global config
+        f = open("config.json")
+        #Parse da susi json momynt
+        t = ""
+        for i in f.readlines():
+            t = t+i.replace("\n","")
+        f.close()
+        config = json.loads(t)
+        del t
+    authorizer = DummyAuthorizer()
+
+    if config["adminuser"]["enabled"]:
+        authorizer.add_user(config["adminuser"]["username"], config["adminuser"]["password"], config["data"], perm='elradfmw')
+
+
+    for i in list(config["users"]):
+        if not i=="forbiden":
+            (config["users"][i]["enabled"])
+            if config["users"][i]["enabled"]:
+                if os.path.exists(config["data"]+"/"+i):
+                    authorizer.add_user(i, config["users"][i]["password"], config["data"]+f"/{i}", perm='elradfmw')
+                else:
+                    print(f"Semms like no folder for {i},creating!")
+                    os.mkdir(f"{config['data']}/{i}")
+                    f=open(f"{config['data']}/{i}/{config['welcome_message']['filename']}","w")
+                    f.close()
+                    f=open(f"{config['data']}/{i}/{config['welcome_message']['filename']}","a")
+                    for o in config['welcome_message']['file-content']:
+                        f.write(o.replace("$USER$",i)+"\n")
+                    f.close()
+                    
+            else:
+                if os.path.exists(f"{config['data']}/forbiden"):
+                    authorizer.add_user(i, config["users"][i]["password"], config["data"]+f"/forbiden", perm='l')
+                else:
+                    os.mkdir(f"{config['data']}/forbiden")
+                    open(f"{config['data']}/forbiden/Your account has been blocked by an administrator","w").close()
+                    authorizer.add_user(i, config["users"][i]["password"], config["data"]+f"/forbiden", perm='l')
+        else:
+            raise InvalidFileException(f"Invalid name: {i}")
+    handler = TLS_FTPHandler
+    handler.certfile = 'server.crt'
+    handler.keyfile = 'server.key'
+
+    handler.authorizer = authorizer
+
+    # Define a customized banner (string returned when client connects)
+    handler.banner = "ready"
+
+    # Optionally specify range of ports to use for passive connections.
+    # handler.passive_ports = range(60000, 65535)
+
+    address = (config["server"]["bindto"], config["server"]["port"])
+    server = FTPServer(address, handler)
+
+    server.max_cons = config["server"]["maxcons"]
+    server.max_cons_per_ip = config["server"]["maxipcons"]
+    (list(config["users"]))
+    server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
